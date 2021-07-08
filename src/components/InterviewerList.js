5382a7e446cca3f004d328398241f3f10e86a7e0
lucasjohannson <lucas.johannson1@ucalgary.ca>
2021-07-07 18:59:21 -0600

    interviewer List and items added

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+//PROPS
+// interviewers:array - an array of objects containing the information of each interviewer
+// interviewer:number - the id of an interviewer
+// setInterviewer:function - a function that accepts an interviewer id
+
+export default function InterviewerList(props) {
+  
+
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  )
+}
